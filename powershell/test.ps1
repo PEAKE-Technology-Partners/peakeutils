bf22cd0a9649cf8d4b561dafb49f64b378df8ed4
Jonathon Rogers <jrogers@peaketechnology.com>
2022-11-23 21:51:07 -0500

    update test script

--- powershell/test.ps1
+++ powershell/test.ps1
@@ -3,9 +3,13 @@ param([string]$name = "world", [switch]$getdate)
 if ($getdate) {
   Get-Date
 }
+$filepath = "C:\temp"
+if($PSVersionTable.Platform -eq "Unix") {
+  $filepath = "/tmp"
+}
 try {
-  New-Item -ItemType "Directory" -Path "C:\temp\" -Force
-  Add-Content -Path "C:\temp\test.log" -Value "ran test script at $(get-date)"
+  New-Item -ItemType "Directory" -Path $filepath -Force
+  Add-Content -Path $(Join-Path $filepath "test.log") -Value "ran test script at $(get-date)"
 }
 catch {
   Write-Host "error: "
